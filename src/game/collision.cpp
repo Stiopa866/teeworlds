a00bdc9099158f90e068bef79ba16d6ef7ada93b
Stiopa866 <stiopagaming@gmail.com>
2020-09-27 18:47:08 +0200

    Ninja Change

--- src/game/collision.cpp
+++ src/game/collision.cpp
@@ -215,3 +215,75 @@ void CCollision::MoveBox(vec2 *pInoutPos, vec2 *pInoutVel, vec2 Size, float Elas
 	*pInoutPos = Pos;
 	*pInoutVel = Vel;
 }
+void CCollision::MoveWaterBox(vec2* pInoutPos, vec2* pInoutVel, vec2 Size, float Elasticity, bool* pDeath) const
+{
+	// do the move
+	vec2 Pos = *pInoutPos;
+	vec2 Vel = *pInoutVel;
+
+	float Distance = length(Vel);
+	int Max = (int)Distance;
+
+	if (pDeath)
+		*pDeath = false;
+
+	if (Distance > 0.00001f)
+	{
+		//vec2 old_pos = pos;
+		float Fraction = 1.0f / (float)(Max + 1);
+		for (int i = 0; i <= Max; i++)
+		{
+			//float amount = i/(float)max;
+			//if(max == 0)
+				//amount = 0;
+
+			vec2 NewPos = Pos + Vel * Fraction; // TODO: this row is not nice
+
+			//You hit a deathtile, congrats to that :)
+			//Deathtiles are a bit smaller
+			if (pDeath && TestBox(vec2(NewPos.x, NewPos.y), Size * (2.0f / 3.0f), COLFLAG_DEATH))
+			{
+				*pDeath = true;
+			}
+			//You are in water
+			if (TestBox(vec2(NewPos.x, NewPos.y), Size * (2.0f / 3.0f), COLFLAG_WATER))
+			{
+				Vel.x *= 0.95;
+				Vel.y *= 0.95;
+			}
+			if (TestBox(vec2(NewPos.x, NewPos.y), Size))
+			{
+				int Hits = 0;
+
+				if (TestBox(vec2(Pos.x, NewPos.y), Size))
+				{
+					NewPos.y = Pos.y;
+					Vel.y *= -Elasticity;
+					Hits++;
+				}
+
+				if (TestBox(vec2(NewPos.x, Pos.y), Size))
+				{
+					NewPos.x = Pos.x;
+					Vel.x *= -Elasticity;
+					Hits++;
+				}
+
+				// neither of the tests got a collision.
+				// this is a real _corner case_!
+				if (Hits == 0)
+				{
+					NewPos.y = Pos.y;
+					Vel.y *= -Elasticity;
+					NewPos.x = Pos.x;
+					Vel.x *= -Elasticity;
+				}
+			}
+
+			Pos = NewPos;
+		}
+	}
+
+	*pInoutPos = Pos;
+	*pInoutVel = Vel;
+}
